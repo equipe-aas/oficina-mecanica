459e4204ce0fcf5cfc8367ef26b2ec060dbdd759
saucesar <sau.cesarlima2013@gmail.com>
2019-05-19 21:55:23 -0300

    Adiçao de endereco em funcionario

--- entidades/Funcionario.py
+++ entidades/Funcionario.py
@@ -1,12 +1,13 @@
 class Funcionario:
-    def __init__(self, rg, cpf, nome, funcao, data_nasc, salario):
+    def __init__(self, rg, cpf, nome, funcao, data_nasc, salario,endereco):
         self.rg = rg
         self.cpf = cpf
         self.nome = nome
         self.funcao = funcao
         self.data_nasc = data_nasc
         self.salario = salario
+        self.endereco = endereco
     def str(self):
         string = "RG: "+str(self.rg)+"\nCPF: "+str(self.cpf)+"\nNOME: "+self.nome+"\nFUNCAO: "+self.funcao+\
-                 "DATA NASCIMENTO: "+str(self.data_nasc)+"SALARIO: R$ "+round(self.salario,2)
+                 "DATA NASCIMENTO: "+str(self.data_nasc)+"SALARIO: R$ "+round(self.salario,2) + "\n"+ self.endereco.str()
         return string
