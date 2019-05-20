95a463364f07c85cd21b1c9ca435201995e4b179
saucesar <sau.cesarlima2013@gmail.com>
2019-05-19 23:07:36 -0300

    Correcao no metodo de busca

--- dados/RepositorioFornecedor.py
+++ dados/RepositorioFornecedor.py
@@ -1,14 +1,14 @@
-class RepositorioServico:
+class RepositorioFornecedor:
     def __init__(self):
         self.fornecedores = []
     def adicionar(self,fornecedor):
         self.fornecedores.append(fornecedor)
     def remover(self,fornecedor):
         self.fornecedores.remove(fornecedor)
-    def buscar(self,codigo):
+    def buscar(self,cnpj):
         fornecedor = None
         for f in self.fornecedores:
-            if f.codigo == codigo:
+            if f.cnpj == cnpj:
                 fornecedor = f
                 break
         return fornecedor
