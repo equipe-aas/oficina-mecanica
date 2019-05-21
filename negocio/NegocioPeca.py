b83a682e2ab3f74f484f54a05e314d61b7daf9a3
saucesar <sau.cesarlima2013@gmail.com>
2019-05-21 13:58:00 -0300

    Criação de novas excessões e criação de Negocio Peca

---
+++ negocio/NegocioPeca.py
@@ -0,0 +1,27 @@
+from dados.RepositorioPeca import RepositorioPeca
+from excessoes.CodigoJaExisteException import CodigoJaExisteException
+from excessoes.CodigoNaoEncontradoException import CodigoNaoEncontradoException
+
+class NegocioPeca:
+    def __init__(self):
+        self.pecas = RepositorioPeca()
+    def adicionar(self,peca):
+        if(self.pecas.buscar(peca.codigo) == None):
+            self.pecas.adicionar(peca)
+        else:
+            raise CodigoJaExisteException(peca.codigo)
+    def remover(self,codigo):
+        peca = self.pecas.buscar(codigo)
+        if(peca != None):
+            self.pecas.remover(peca)
+        else:
+            raise CodigoNaoEncontradoException(codigo)
+    def buscar(self, codigo):
+        peca = self.pecas.buscar(codigo)
+        if (peca != None):
+            return peca
+        else:
+            raise CodigoNaoEncontradoException(codigo)
+
+    def str(self):
+        return self.pecas.str()
