c7d8f357e3411ca546798b6f562afd19ab111282
Sau Cesar <sau.cesarlima2013@gmail.com>
2019-05-30 22:24:38 -0300

    Mudanca no metodo de adicionar

--- negocio/NegocioCliente.py
+++ negocio/NegocioCliente.py
@@ -5,13 +5,14 @@ from excessoes.CpfInvalidoException import CpfInvalidoException
 from excessoes.NomeInvalidoException import NomeInvalidoException
 from excessoes.TelefoneInvalidoException import TelefoneInvalidoException
 from entidades.Cliente import Cliente
+from validacao.ValidaDados import ValidaDados
 
 class NegocioCliente:
     def __init__(self):
         self.clientes = RepositorioPessoa()
     def adicionar(self, cpf, nome, endereco, telefone):
-        if not self.isCpf(cpf):
-            raise CpfInvalidoException(cpf)
+        if not ValidaDados.__isCpf__(cpf):
+            raise    CpfInvalidoException(cpf)
         if(self.clientes.buscar(cpf) != None):
             raise CpfJaExisteException(cpf)
         if len(nome)<10:
@@ -34,12 +35,4 @@ class NegocioCliente:
             raise CpfNaoEncontradoException(cpf)
     def __str__(self):
         return self.clientes.__str__()
-    def isCpf(self, cpf):
-        if(len(cpf) < 11):
-            return False
 
-        n = ['0','1','2','3','4','5','6','7','8','9']
-        for i in list(cpf):
-            if(i not in n):
-                return False
-        return True
--- negocio/NegocioFuncionario.py
+++ negocio/NegocioFuncionario.py
@@ -1,15 +1,32 @@
 from dados.RepositorioPessoa import RepositorioPessoa
 from excessoes.CpfJaExisteException import CpfJaExisteException
 from excessoes.CpfNaoEncontradoException import CpfNaoEncontradoException
+from excessoes.RgExisteException import RgExisteException
+from excessoes.NomeInvalidoException import NomeInvalidoException
+from excessoes.ValorInvalidoException import ValorInvalidoException
+from excessoes.TelefoneInvalidoException import TelefoneInvalidoException
+from validacao.ValidaDados import ValidaDados
+from entidades.Funcionario import Funcionario
 
-class NegocioCliente:
+class NegocioFuncionario:
+    matricula = 0
     def __init__(self):
         self.funcionarios = RepositorioPessoa()
-    def adicionar(self,funcionario):
-        if(self.funcionarios.buscar(funcionario.cpf) != None):
-            self.adicionar(funcionario)
+    def adicionar(self, rg, cpf, nome, funcao, data_nasc, salario,endereco,telefone):
+        if self.funcionarios.buscar(cpf) != None:
+            raise CpfJaExisteException(cpf)
+        if self.buscarPorRg(rg) != None:
+            raise RgExisteException(rg)
+        if len(nome) < 5:
+            raise NomeInvalidoException(nome)
+        if(salario < 0):
+            raise ValorInvalidoException(salario)
+        if(len(telefone) < 8 or not ValidaDados.__isNumeric__(telefone)):
+            raise TelefoneInvalidoException(telefone)
         else:
-            raise CpfJaExisteException(funcionario.cpf)
+            NegocioFuncionario.matricula += 1
+            self.funcionarios.adicionar(Funcionario(NegocioFuncionario.matricula,rg, cpf, nome, funcao,
+                                                    data_nasc, salario, endereco, telefone))
     def remover(self,cpf):
         funcionario = self.funcionarios.buscar(cpf)
         if(funcionario != None):
@@ -22,6 +39,13 @@ class NegocioCliente:
             return funcionario
         else:
             raise CpfNaoEncontradoException(cpf)
+    def buscarPorRg(self,rg):
+        funcionario = None
+        for i in self.funcionarios.pessoas:
+            if i.rg == rg:
+                funcionario = i
+                break
+        return funcionario
     def promover(self,cpf):
         funcinario = self.buscar(cpf)
         if(funcinario != None):
@@ -29,5 +53,5 @@ class NegocioCliente:
             funcinario.funcao = "GERENTE"
         else:
             raise CpfNaoEncontradoException(cpf)
-    def str(self):
-        return self.funcionarios.str()
+    def __str__(self):
+        return self.funcionarios.__str__()
