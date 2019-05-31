2a80b74acaca179e668e9c2580c3ecb5c35fe3b8
Sau Cesar <sau.cesarlima2013@gmail.com>
2019-05-30 22:22:40 -0300

    Adicao do parametro matricula

--- entidades/Funcionario.py
+++ entidades/Funcionario.py
@@ -1,7 +1,8 @@
 from entidades.Pessoa import Pessoa
 class Funcionario(Pessoa):
-    def __init__(self, rg, cpf, nome, funcao, data_nasc, salario,endereco,telefone):
+    def __init__(self, matricula, rg, cpf, nome, funcao, data_nasc, salario,endereco,telefone):
         Pessoa.__init__(cpf, nome, endereco, telefone)
+        self.matricula = matricula
         self.rg = rg
         self.funcao = funcao
         self.data_nasc = data_nasc
@@ -12,6 +13,7 @@ class Funcionario(Pessoa):
             self.is_gerente = False
 
     def __str__(self):
-        string = "RG: "+str(self.rg)+"\nCPF: "+str(self.cpf)+"\nNOME: "+self.nome+"\nFUNCAO: "+self.funcao+\
-                 "DATA NASCIMENTO: "+str(self.data_nasc)+"SALARIO: R$ "+round(self.salario,2) + "\n"+ self.endereco.str()
+        string = "MATRICULA: "+str(self.matricula)+"\nRG: "+str(self.rg)+"\nCPF: "+str(self.cpf)+"\nNOME: "+self.nome+\
+                 "\nFUNCAO: "+self.funcao+"DATA NASCIMENTO: "+str(self.data_nasc)+"SALARIO: R$ "+round(self.salario,2)+\
+                 "\n"+self.endereco.str()
         return string
