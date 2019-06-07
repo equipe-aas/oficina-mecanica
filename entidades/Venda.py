8341fd1291f865bac5eec4368c3fcda9e48401f6
Sau Cesar <sau.cesarlima2013@gmail.com>
2019-06-07 20:39:27 -0300

    venda modificada para aceitar apenas pecas

--- entidades/Venda.py
+++ entidades/Venda.py
@@ -1,29 +1,27 @@
 from datetime import datetime
-from entidades.ItemVenda import ItemVenda
+from entidades.PecaQuantidade import PecaQuantidade
 
 
 class Venda:
-    def __init__(self,codigo):
+    def __init__(self,codigo,pecas=[]):
         self.codigo = codigo
-        self.itens = []
+        self.pecas = pecas
         self.data = datetime.today().strftime('%d/%m/%Y')
         self.total = 0
-    def adicionarItem(self,item,quantidade):
-        if(type(item) == "Peca"):
-            self.itens.append(ItemVenda(quantidade, item, None))
-        else:
-            self.itens.append(ItemVenda(quantidade, None, item))
-        self.total += item.preco_venda
+    def adicionarItem(self,peca,quantidade):
+        pecaQuant = PecaQuantidade(peca, quantidade)
+        self.pecas.append(pecaQuant)
+        self.total += pecaQuant.total()
     def removerItem(self,codigo):
-        for i in self.itens:
+        for i in self.pecas:
             if i.codigo == codigo:
-                self.total += i.preco_venda
-                self.itens.remove(i)
+                self.total -= i.total
+                self.pecas.remove(i)
                 break
     def __str__(self):
         string = ""
         total= 0
-        for i in self.itens:
-            string += i.calcularTotal()
+        for i in self.pecas:
+            string += i.total()
         string += "\nTOTAL: R$ "+str(total)
         return string
