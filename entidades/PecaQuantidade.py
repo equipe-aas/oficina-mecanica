a688c2cb48adc7ff3ec554c127947129a31dd924
Sau Cesar <sau.cesarlima2013@gmail.com>
2019-06-07 20:41:59 -0300

    Adicionado preco de compra e venda em peca quantidade para gerar relatorios corretamente

--- entidades/PecaQuantidade.py
+++ entidades/PecaQuantidade.py
@@ -1,8 +1,12 @@
 class PecaQuantidade:
     def __init__(self, peca, quantidade):
         self.peca = peca
+        self.preco_custo = peca.preco_custo
+        self.preco_venda = peca.preco_venda
         self.quantidade = quantidade
     def __str__(self):
         string = "CODIGO "+str(self.peca.codigo)+" DESCRICAO: "+str(self.peca.descricao)+\
-                 " QUANTIDADE: "+str(self.peca.quantidade) +"SUBTOTAL: R$ "+ round(self.quantidade*self.peca.preco_venda,2)
+                 " QUANTIDADE: "+str(self.quantidade) +"SUBTOTAL: R$ "+ str(round(self.quantidade*self.preco_venda,2))
         return string
+    def total(self):
+        return round(self.quantidade*self.preco_venda,2)
