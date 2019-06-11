15a60475366a7842d7cfcc3feab3bc6baaf1c73b
Sau Cesar <sau.cesarlima2013@gmail.com>
2019-06-11 00:38:38 -0300

    conserto no construtor e em str()

--- negocio/entidades/Cliente.py
+++ negocio/entidades/Cliente.py
@@ -1,4 +1,10 @@
-from negocio.entidades.Pessoa import Pessoa
-class Cliente(Pessoa):
+class Cliente:
     def __init__(self, cpf, nome, endereco, telefone):
-        Pessoa.__init__(self,cpf,nome,endereco,telefone)
+        self.cpf = cpf
+        self.nome = nome
+        self.endereco = endereco
+        self.telefone = telefone
+    def __str__(self):
+        string = "\nCPF: "+str(self.cpf)+"\nNOME: "+self.nome+"\nTELEFONE(S): "+\
+                 self.telefone+"\nENDERECO: "+self.endereco
+        return string
